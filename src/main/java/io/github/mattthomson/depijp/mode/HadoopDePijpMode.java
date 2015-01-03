3543eb90d66a85926cdf2587dccde5ca61b81413
Matt Thomson <mattjohnthomson@gmail.com>
2015-01-03 17:23:18 +0000

    Use Kryo for serialization.

--- src/main/java/io/github/mattthomson/depijp/mode/HadoopDePijpMode.java
+++ src/main/java/io/github/mattthomson/depijp/mode/HadoopDePijpMode.java
@@ -2,14 +2,18 @@ package io.github.mattthomson.depijp.mode;
 
 import cascading.flow.FlowConnector;
 import cascading.flow.hadoop2.Hadoop2MR1FlowConnector;
+import cascading.kryo.KryoSerialization;
 import cascading.tap.Tap;
+import com.google.common.collect.ImmutableMap;
 import io.github.mattthomson.depijp.DePijpSink;
 import io.github.mattthomson.depijp.DePijpSource;
 
 public class HadoopDePijpMode implements DePijpMode {
     @Override
     public FlowConnector createFlowConnector() {
-        return new Hadoop2MR1FlowConnector();
+        return new Hadoop2MR1FlowConnector(ImmutableMap.of(
+                "io.serializations", KryoSerialization.class.getName()
+        ));
     }
 
     @Override
